cadd19c5360966c3bbaf23eabdf975b2e0ec25f9
lauraclare15 <113399473+lauraclare15@users.noreply.github.com>
2022-10-20 19:24:34 -0400

    Game is fully working!

--- Portfolio/Java/rps/script.js
+++ Portfolio/Java/rps/script.js
@@ -1,27 +1,31 @@
 const playerChoice = document.querySelector('.playerChoice');
-const btn1 = document.querySelector('.bttn1');
+const compChoice = document.querySelector('.compChoice');
+const resultIs = document.querySelector('.result');
 
-btn1.addEventListener('click', playerChooses());
+const pchoice = document.querySelector('.bttn1');
+
 
 function playerChooses(pchoice) {
     
     if (pchoice === 'rock') {
         playerChoice.textContent = 'You have chosen ROCK';
+        compChooses();
+        runGame();
+
     } else if (pchoice === 'paper') {
         playerChoice.textContent = 'You have chosen PAPER';
+        compChooses();
+        runGame();
+
     } else if (pchoice === 'scissors') {
         playerChoice.textContent = 'You have chosen SCISSORS';
+        compChooses();
+        runGame();
     }
 }
 
-const compChoice = document.querySelector('.compChoice');
-const btn2 = document.querySelector('.readybttn');
-
-btn2.addEventListener('click', compChooses());
-
 function compChooses() {
-    let cchoice = Math.floor(Math.random() * 3);
-    
+   let cchoice = Math.floor(Math.random() * 3); 
     if (cchoice === 0) {
         compChoice.textContent = 'The Computer has chosen ROCK';
     } else if (cchoice === 1) {
@@ -30,3 +34,25 @@ function compChooses() {
         compChoice.textContent = 'The Computer has chosen SCISSORS';
     }
 };
+
+function runGame() {
+    if (
+        (compChoice.textContent === 'The Computer has chosen ROCK' && playerChoice.textContent === 'You have chosen ROCK')||  
+        (compChoice.textContent === 'The Computer has chosen PAPER' && playerChoice.textContent === 'You have chosen PAPER')||
+        (compChoice.textContent === 'The Computer has chosen SCISSORS' && playerChoice.textContent === 'You have chosen SCISSORS')
+        ){
+        resultIs.textContent = 'It\'s a Tie'
+    } else if (
+        (compChoice.textContent === 'The Computer has chosen ROCK' && playerChoice.textContent === 'You have chosen SCISSORS')|| 
+        (compChoice.textContent === 'The Computer has chosen PAPER' && playerChoice.textContent === 'You have chosen ROCK')||
+        (compChoice.textContent === 'The Computer has chosen SCISSORS' && playerChoice.textContent === 'You have chosen PAPER')
+        ){
+        resultIs.textContent = 'Computer Wins'
+    } else if (
+        (compChoice.textContent === 'The Computer has chosen ROCK' && playerChoice.textContent === 'You have chosen PAPER')|| 
+        (compChoice.textContent === 'The Computer has chosen PAPER' && playerChoice.textContent === 'You have chosen SCISSORS')|| 
+        (compChoice.textContent === 'The Computer has chosen SCISSORS' && playerChoice.textContent === 'You have chosen ROCK')
+        ){
+        resultIs.textContent = 'Player Wins'
+    }
+}
